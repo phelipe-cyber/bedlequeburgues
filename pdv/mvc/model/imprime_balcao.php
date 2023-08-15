8ac8bfdea445f92d78c093f38f92582cd3ca229b
phelipe-cyber <phelipe.silveira@trocafone.com>
2023-08-14 22:26:46 -0300

    imprimir

--- pdv/mvc/model/imprime_balcao.php
+++ pdv/mvc/model/imprime_balcao.php
@@ -9,17 +9,17 @@ use Mike42\Escpos\PrintConnectors\FilePrintConnector;
 // $printer = new Printer(new FilePrintConnector("php://stdout"));
 $connector = new FilePrintConnector("php://stdout");
 $printer = new Printer($connector);
-print_r($printer);
-echo "<br>";
-print_r($$connector);
+// echo "<br>";
+// print_r($$connector);
 
 try {
     // Carrega o conteúdo do arquivo HTML
-    $htmlContent = file_get_contents('imprimir.php');
-
+    $htmlContent = file_get_contents('/imprimir.php');
+    
     // Imprime o conteúdo na impressora
     $printer->textRaw($htmlContent);
-
+    print_r($printer);
+    
     // Corte de papel (opcional)
     $printer->cut();
     
