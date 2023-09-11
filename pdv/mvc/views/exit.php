fba123157f9f1b8f303ef4d873462e99ce8675e8
unknown <phelipe.silveira@trocafone.com>
2023-09-10 22:12:19 -0300

    venda do dia e despesas

--- pdv/mvc/views/exit.php
+++ pdv/mvc/views/exit.php
@@ -1,3 +1,21 @@
+<?php
+include "./mvc/model/conexao.php";
+
+date_default_timezone_set('America/Sao_Paulo');
+$data_hoje = date('d/m/Y');
+
+
+$tab_caixa = "SELECT pgto 'pgto', SUM(valor) 'valor', SUM(valor_maquina) 'valor_maquina' , data FROM `vendas` 
+WHERE data = '$data_hoje'
+GROUP by pgto;";
+$caixa = mysqli_query($conn, $tab_caixa);
+
+$tab_despesa = "SELECT SUM(valor) valor, data FROM `despesas` where data = '$data_hoje' GROUP by DATA";
+
+$despesa = mysqli_query($conn, $tab_despesa);
+
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -50,5 +68,128 @@
         
         </script>
     </form>
+
+<!-- Font Awesome -->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+<!-- Bootstrap core CSS -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+<!-- Material Design Bootstrap -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.2/css/mdb.min.css" rel="stylesheet">
+<!-- <link href="https://cdn.datatables.net/1.10.18/css/dataTables.bootstrap4.min.css" rel="stylesheet"> -->
+<!-- JQuery -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<!-- Bootstrap tooltips -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
+<!-- Bootstrap core JavaScript -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<!-- MDB core JavaScript -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.2/js/mdb.min.js"></script>
+<!-- <script type="text/javascript" src="https://cdn.datatables.net/1.10.18/js/jquery.dataTables.min.js"></script> -->
+<script type="text/javascript" src="https://cdn.datatables.net/1.10.18/js/dataTables.bootstrap4.min.js"></script>
+
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.12.1/datatables.min.css" />
+
+<script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.12.1/datatables.min.js"></script>
+<h1>Vendas do dia</h1>
+<div class="table-responsive">
+    <!-- <div class="col-2"> -->
+    <!-- <div class="flex-center flex-column"> -->
+    <!-- <div class="card card-body"> -->
+
+    <!-- <div class="table-responsive"> -->
+    <table id="dtBasicExample" class="table table-striped table-bordered table-sm reponsive" cellspacing="0"
+        width="100%">
+        <!-- <table id="dtBasicExample" class="table table-striped table-bordered table-sm" cellspacing="0" width="100%"> -->
+        <thead>
+            <tr>
+                <th>Tipo de pagamento</th>
+                <th>Valor</th>
+                <th>Valor Maquininha</th>
+                <th>Data</th>
+
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+                    $index = 0;
+                    while ($rows_caixa = mysqli_fetch_assoc($caixa)) { 
+                    ?>
+            <tr>
+
+                <td><?php echo $rows_caixa['pgto'] ?></td>
+				<td>R$ <?php echo number_format($rows_caixa['valor'], 2); ?></td>
+				<td>R$ <?php echo number_format($rows_caixa['valor_maquina'], 2); ?></td>
+				<td><?php echo $rows_caixa['data'] ?></td>
+               
+            </tr>
+
+
+            <?php $index++;
+                    } ?>
+
+
+        </tbody>
+    </table>
+</div>
+
+<h1>Despesas do dia</h1>
+<div class="table-responsive">
+    <table id="dtBasicExample2" class="table table-striped table-bordered table-sm reponsive" cellspacing="0" width="100%">
+        <thead>
+            <tr>
+                <th>Valor despesa total</th>
+                <th>Data</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+                    $index = 0;
+                    while ($rows_despesa = mysqli_fetch_assoc($despesa)) { 
+                    ?>
+            <tr>
+
+               <td>R$ <?php echo number_format($rows_despesa['valor'], 2); ?></td>
+               <td> <?php echo $rows_despesa['data'] ?></td>
+               
+            </tr>
+
+
+            <?php $index++;
+                    } ?>
+
+
+        </tbody>
+    </table>
+</div>
+
+<script>
+$(document).ready(function() {
+    $('#dtBasicExample').DataTable({
+        "paging": false, // false to disable pagination (or any other option)
+        "ordering": false, // false to disable sorting (or any other option)
+        "searching": false,
+        "language": {
+            "url": "https://cdn.datatables.net/plug-ins/1.12.1/i18n/pt-BR.json"
+        }
+    });
+    $('.dataTables_length').addClass('bs-select');
+});
+</script>
+<script>
+$(document).ready(function() {
+    $('#dtBasicExample2').DataTable({
+        "paging": false, // false to disable pagination (or any other option)
+        "ordering": false, // false to disable sorting (or any other option)
+        "searching": false,
+        "language": {
+            "url": "https://cdn.datatables.net/plug-ins/1.12.1/i18n/pt-BR.json"
+        }
+    });
+    $('.dataTables_length').addClass('bs-select');
+});
+</script>
+
+
+
 </body>
 </html>
