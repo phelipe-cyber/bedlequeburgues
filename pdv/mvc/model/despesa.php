953f174596ffdd3a365b9a2263eb26a542e9e2d8
unknown <phelipe.silveira@trocafone.com>
2023-10-27 20:34:33 -0300

    voltar para despesas

--- pdv/mvc/model/despesa.php
+++ pdv/mvc/model/despesa.php
@@ -44,15 +44,15 @@ $updatequantidade = mysqli_query($conn, $update);
 
 	<?php
 
-	// if(mysqli_affected_rows($conn)!=-1){
+	if(mysqli_affected_rows($conn)!=-1){
 
-	// 	echo "<META HTTP-EQUIV=REFRESH CONTENT = '0;URL=/pdv/?view=despesas'>";
-	// 	$_SESSION['msg'] = "<div class='alert alert-success' role='alert'>Despesa Cadastrada com Sucesso<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button></div>";
-	// }else{
+		echo "<META HTTP-EQUIV=REFRESH CONTENT = '0;URL=/pdv/?view=despesas'>";
+		$_SESSION['msg'] = "<div class='alert alert-success' role='alert'>Despesa Cadastrada com Sucesso<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button></div>";
+	}else{
 
-	// 	echo "<META HTTP-EQUIV=REFRESH CONTENT = '0;URL=/pdv/?view=despesas'>";	
-	// 	$_SESSION['msg'] = "<div class='alert alert-danger' role='alert'>Erro ao Cadastrar Despesa <button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button></div>";
-	// }?>
+		echo "<META HTTP-EQUIV=REFRESH CONTENT = '0;URL=/pdv/?view=despesas'>";	
+		$_SESSION['msg'] = "<div class='alert alert-danger' role='alert'>Erro ao Cadastrar Despesa <button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button></div>";
+	}?>
 
 		
 	</body>
