9b53411d0a36a44abe5801cb0377dd68c3c76896
unknown <phelipe.silveira@trocafone.com>
2024-02-08 21:39:06 -0300

    ponto

--- pdv/mvc/common/header.php
+++ pdv/mvc/common/header.php
@@ -189,6 +189,7 @@ $usuario = $_SESSION['user'];
             <a class="collapse-item" href="/pdv/?view=produtos" style=" border-radius: 8px; font-size:18px;">Vendas Produtos</a>
             <a class="collapse-item" href="/pdv/?view=despesas" style=" border-radius: 8px; font-size:18px;">Add Despesas</a>
             <a class="collapse-item" href="/pdv/?view=fiado" style=" border-radius: 8px; font-size:18px;">Fiados</a>
+            <a class="collapse-item" href="/pdv/?view=ponto" style=" border-radius: 8px; font-size:18px;">Ponto</a>
 
           </div>
         </div>
